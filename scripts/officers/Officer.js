a755e4fe612e79b921455bba8bebb26dedbd3ea6
Parker Kelley <Pklly92@gmail.com>
2020-08-31 11:17:09 -0500

    it's pushing the officers

--- scripts/officers/Officer.js
+++ scripts/officers/Officer.js
@@ -1,7 +1,7 @@
 export const OfficerHTML = (officerObj) => {
 	return `
     <section section class="officer-card" id="officer-id-${officerObj.id}" >
-      <h2>${officerObj.name}</h2>
+      <h2>Officer: ${officerObj.name}</h2>
 
     </section>
   `;
--- scripts/officers/OfficerList.js
+++ scripts/officers/OfficerList.js
@@ -4,6 +4,16 @@ import { OfficerHTML } from './Officer.js';
 export const OfficerList = () => {
 	getOfficers().then(() => {
 		const OfficerArray = useOfficers();
-		console.log('This should be an officer array', OfficerArray);
+		addOfficersToDOM(OfficerArray);
 	});
+};
+
+const addOfficersToDOM = (anOfficerArray) => {
+	const domElement = document.querySelector('.Officer-Container');
+
+	let HTMLArray = anOfficerArray.map((officerObj) => {
+		return OfficerHTML(officerObj);
+	});
+
+	domElement.innerHTML = HTMLArray.join('');
 };
